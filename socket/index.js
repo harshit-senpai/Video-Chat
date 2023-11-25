925b0e0207ffe816d8f53eb07a44ca26d443176e
harshit Mishra <miharshit2@gmail.com>
2023-11-25 20:44:34 +0530

    Socket ready

--- socket/index.js
+++ socket/index.js
@@ -1,8 +1,39 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-const server = require('http').createServer(app);
+const server = require("http").createServer(app);
 
-const cors = require('cors')
+const cors = require("cors");
 
-const 
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Server is Running...");
+});
+
+io.on("connection", (socket) => {
+  socket.emit("me", socket.id);
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("call ended");
+  });
+
+  socket.on("calluser", ({ userToCall, signalData, from, name }) => {
+    io.to(userToCall), emit("calluser", { signal: signalData, from, name });
+  });
+
+  socket.on("answeecall", (data) => {
+    io.to(data.to).emit("callaccepted", data.signal);
+  });
+});
+
+server.listen(PORT, () => console.log(`server is listening on ${PORT} `));
